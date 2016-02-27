06880c6f849eefbd5cb87f0d018147918845c7a1
jefffyangdis <fangyang@fangyangdeMacBook-Pro.local>
2016-02-27 17:58:51 +0800

    在.lldbinit中添加脚本命令配置
    通过在.lldbinit中进入python文件导入，同时在python模块被导入时添加lldb命令

--- inGivenFrame.py
+++ inGivenFrame.py
@@ -3,7 +3,7 @@ import lldb
 #used like this
 #(lldb) command script import ~/lldb/inGivenFrame.py
 #(lldb) br s -l codeline
-#(lldb) br comm add --script-type python -o "inGivenFrame.inGivenFrame(frame,bp_loc,'givenfuncname')" 1
+#(lldb) br comm add --script-type python -o "inGivenFrame.isInGivenFrame(frame,bp_loc,'givenfuncname')" 1
 #(lldb) br comm add --script-type python -F inGivenFrame.inGivenFrameF 1
 
 def inGivenFrame(frame,location,givenFrameName):
@@ -32,4 +32,8 @@ def inGivenFrameF(frame,location,unused):
 			break
 	if bIsInGivenFrame != True:
 		process.Continue()
+def hello(debugger,command,result,dict):
+	print 'hello'
 
+def __lldb_init_module(debugger,dict):
+	debugger.HandleCommand('command script add -f inGivenFrame.hello hello')
